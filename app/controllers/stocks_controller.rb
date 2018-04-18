07ba30dada28bb8bcec672c3c523b548ee54a3f2
Michael Hartman <mhartm16@uncc.edu>
2018-04-18 21:21:54 +0000

    Complete Create and Show

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,20 @@
 class StocksController < ApplicationController
+    def show
+        @stock = Stock.find(params[:id])
+    end
+    
     def new
     end
     
     def create
-        render plain: params[:stocks].inspect
+        @stock = Stock.new(stock_params)
+        
+        @stock.save
+        redirect_to @stock
     end
 end
+
+private 
+    def stock_params
+        params.require(:stocks).permit(:title)
+    end
